bf04b53867b2fbd51adc01c300f25a77d3521e8f
Elias <eliasgalindo@gmail.com>
2022-02-02 22:31:19 -0300

    Fix names on first function - fetchProducts

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,11 +1,12 @@
+// código feito com o auxílio do aluno Miguel Lima Tribo-B
 const fetchProducts = async (query) => {
   if (!query) {
     return new Error('You must provide an url');
   }
   const url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
-  const xablau = await fetch(url);
-  const xabalu = await xablau.json();
-  return xabalu;
+  const result = await fetch(url);
+  const data = await result.json();
+  return data;
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -2,31 +2,30 @@ require('../mocks/fetchSimulator');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
-describe('1 - Teste a função fetchProducts', () => {
-  it('Teste se fetchProducts é uma função;',() => {
+describe('Teste a função fetchProducts', () => {
+  it('1. Teste se `fetchProducts` é uma função;',() => {
   expect( typeof fetchProducts).toBe('function')
   })
   
-  it('Execute a função fetchProducts com o argumento "computador" e teste se fetch foi chamada' , async () => {
+  it('2. Execute a função `fetchProducts` com o argumento "computador" e teste se `fetch` foi chamada;' , async () => {
   await fetchProducts('computador')
   expect(fetch).toHaveBeenCalled()
   })
   
-  it('Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch a API do mercado livre;' , async () => {
+  it('3. Teste se, ao chamar a função `fetchProducts` com o argumento "computador", a função `fetch` utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador";' , async () => {
   const url = "https://api.mercadolibre.com/sites/MLB/search?q=computador"
   await fetchProducts('computador')
   expect(fetch).toHaveBeenCalledWith(url)
   })
   
-  it('Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+  it('4. Teste se o retorno da função `fetchProducts` com o argumento "computador" é uma estrutura de dados igual ao objeto `computadorSearch`, que já está importado no arquivo.', async () => {
   const result = await fetchProducts('computador')
   expect(result).toEqual(computadorSearch)
   })
   
-  it('Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: "You must provide an url".', async () => {
+  it('5. Teste se, ao chamar a função `fetchProducts` sem argumento, retorna um erro com a mensagem: `You must provide an url`.', async () => {
     const erroMessage = new Error('You must provide an url')
     const result = await fetchProducts()
     expect(result).toEqual(erroMessage) 
-    })
-    
-    }); 
+  })
+}); 
